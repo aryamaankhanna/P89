f006365f3c4167ea360317c3f2f24a8ec21ca107
aryamaankhanna <76615706+aryamaankhanna@users.noreply.github.com>
2021-04-11 12:28:01 +0530

    Add files via upload

---
+++ p 89/math quiz/game_page.js
@@ -0,0 +1,24 @@
+
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score= 0;
+player2_score= 0;
+
+document.getElementById("player1_name").innerHTML= player1_name + ":";
+document.getElementById("player2_name").innerHTML= player2_name + ":";
+
+document.getElementById("player1_score").innerHTML= player1_score ;
+document.getElementById("player2_score").innerHTML= player2_score ;
+
+document.getElementById("player_question").innerHTML= "Question Turn -" +player1_name ;
+document.getElementById("player_answer").innerHTML= "Answer Turn -"+ player2_name ;
+
+function send() {
+  var get_number_1=document.getElementById("number1").value;
+ console.log(get_number_1);
+ var get_number_2=document.getElementById("number2").value;
+ console.log(get_number_2);
+ document. getElementById("output"). innerHTML = get_number_1 * get_number_2;
+ 
+}
